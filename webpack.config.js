b8e28a31c2641da1cc8557dffb19c58a9e6e8c49
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-04-19 15:55:19 -0500

    add a css file to the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,18 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
